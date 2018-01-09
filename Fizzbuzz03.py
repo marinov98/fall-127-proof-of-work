1f50af5f6e696a993c21bbb6c398c38df2413200
marinov98 <33673401+marinov98@users.noreply.github.com>
2018-01-08 19:59:47 -0500

    Create Fizzbuzz03.py

---
+++ Fizzbuzz03.py
@@ -0,0 +1,16 @@
+l = []
+def fizzbuzz(start, end):
+    for i in range(start, end + 1):
+        s = ''
+        if i%3 == 0:
+            s += "fizz"
+        if i%5 == 0:
+            s += "buzz"
+        if (i%3 != 0 and i%5 != 0):
+            s += str(i)
+        l.append(s)
+ 
+    for z in result:
+        print(x)
+    
+print(fizzbuzz(1, 50))
