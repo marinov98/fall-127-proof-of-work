76b062570b1f3f379271e4b61fe006112c6bf07b
marinov98 <wbbmarin5@gmail.com>
2018-03-15 11:51:31 -0400

    fizz done

---
+++ personal Python code/fizzq4.py
@@ -0,0 +1,11 @@
+def Fizzbuzz():
+    for i in range(1,101):
+        if i % 3 == 0:
+            print("Fizz")
+        if i % 5 == 0:
+            print("Buzz")
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print(i)
+Fizzbuzz()
